6fcaaa4412f880033f1e1d5514a64bbcc2cf797a
SteamOS User <1027859@student.sae.edu.au>
2026-09-17 21:07:08 +1000

    more Work

---
+++ script.js
@@ -0,0 +1,55 @@
+let contrast = false;
+
+function takeToPage(page) {
+    window.location.href = `./${page}.html`;
+}
+function collapse(elem, cont) {
+    const arrow = elem.querySelector('.ar');
+    const content = document.getElementById(cont);
+
+    content.hidden = !content.hidden;
+
+    if (content.hidden) {
+        arrow.textContent = "▲";
+    } else {
+        arrow.textContent = "▼";
+    }
+}
+
+function callPhone(num){
+    window.open(`tel:${num}`);
+}
+
+function switchContrast(self){
+
+
+    self.classList.toggle('rotate')
+
+    contrast = !contrast;
+    checkContrast()
+}
+
+//window.load(checkContrast())
+
+
+function checkContrast(){
+
+    console.log(`chek, is ${contrast}`);
+    if(contrast){
+        console.log("change contrast");
+        document.body.classList.toggle('alt-mode');
+    }
+    else{
+        document.body.classList.remove('alt-mode');
+    }
+}
+
+function switchText(self,tts, tts2){
+
+    if(self.textContent === tts){
+        self.textContent = tts2;
+    }
+    else{
+        self.textContent = tts;
+    }
+}
